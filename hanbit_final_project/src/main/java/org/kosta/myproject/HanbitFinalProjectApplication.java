6b206fc526514518db3de60427ba1f72c9bc4f15
kimyunsoo56 <kimyunsoo56@gmail.com>
2022-12-14 17:04:16 +0900

    HanBit Final Projest Git 연결

---
+++ hanbit_final_project/src/main/java/org/kosta/myproject/HanbitFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package org.kosta.myproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanbitFinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanbitFinalProjectApplication.class, args);
+	}
+
+}
---
+++ hanbit_final_project/src/main/java/org/kosta/myproject/controller/HomeController.java
@@ -0,0 +1,16 @@
+package org.kosta.myproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	@RequestMapping(value = {"/","home"})
+	public String home() {
+		return "index";
+	}
+	@RequestMapping("noticeBoard")
+	public String noticeBoardList() {
+		return "noticeBoard/noticeBoardList";
+	}
+}
