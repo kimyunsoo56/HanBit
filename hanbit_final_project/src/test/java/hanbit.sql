cbbc4baf72c6fa9b22aa2d3d7a7b39c34db1202a
chaen-ni <cossltkfkd77@naver.com>
2022-12-15 15:02:28 +0900

    박채은-테이블 생성 작업 완료!

--- hanbit_final_project/src/test/java/hanbit.sql
+++ hanbit_final_project/src/test/java/hanbit.sql
@@ -1,3 +1,4 @@
+-- 한빛 회원 테이블 생성
 CREATE TABLE hanbit_member(
 	id	VARCHAR2(100) PRIMARY KEY,
 	password VARCHAR2(100) NOT NULL,
@@ -16,6 +17,7 @@ CREATE TABLE hanbit_member(
 DROP TABLE hanbit_member;
 SELECT * FROM hanbit_member;
 
+-- 한빛 권한 테이블 생성
 CREATE TABLE hanbit_authorities(
 	id VARCHAR2(100) NOT NULL,
 	authority VARCHAR2(30) NOT NULL,
@@ -25,13 +27,96 @@ CREATE TABLE hanbit_authorities(
 DROP TABLE hanbit_authorities;
 SELECT * FROM hanbit_authorities;
 
-CREATE TABLE notice_board(
+-- 한빛 알림 게시판 테이블 생성
+CREATE TABLE hanbit_notice_board(
 	notice_no NUMBER PRIMARY KEY,
 	title VARCHAR2(100) NOT NULL,
 	content CLOB NOT NULL,
-	time_posted DATE DEFAULT SYSDATE(YY-MM-DD HH:mm),
+	time_posted DATE DEFAULT SYSDATE,
 	hits NUMBER DEFAULT 0,
-	category VARCHAR2(100) 
+	category VARCHAR2(100), 
+	image VARCHAR2(100),
+	id VARCHAR2(100) NOT NULL,
+	CONSTRAINT hanbit_notice_board_fk FOREIGN KEY(id) REFERENCES hanbit_member(id)
 )
+CREATE SEQUENCE hanbit_notice_board_seq;
+DROP TABLE hanbit_notice_board;
+DROP SEQUENCE hanbit_notice_board_seq;
+SELECT * FROM hanbit_notice_board;
+
+-- 한빛 자유 게시판 테이블 생성
+CREATE TABLE hanbit_free_board(
+	free_no NUMBER PRIMARY KEY,
+	title VARCHAR2(100) NOT NULL,
+	content CLOB NOT NULL,
+	time_posted DATE DEFAULT SYSDATE,
+	hits NUMBER DEFAULT 0,
+	category VARCHAR2(100), 
+	image VARCHAR2(100),
+	id VARCHAR2(100) NOT NULL,
+	CONSTRAINT hanbit_free_board_fk FOREIGN KEY(id) REFERENCES hanbit_member(id)
+)
+CREATE SEQUENCE hanbit_free_board_seq;
+DROP TABLE hanbit_free_board;
+DROP SEQUENCE hanbit_free_board_seq;
+SELECT * FROM hanbit_free_board;
+
+-- 한빛 자유 댓글 테이블 생성
+CREATE TABLE hanbit_comment(
+	comment_no NUMBER PRIMARY KEY,
+	content CLOB NOT NULL,
+	time_posted DATE DEFAULT SYSDATE,
+	id VARCHAR2(100) NOT NULL,
+	free_no NUMBER NOT NULL,
+	CONSTRAINT hanbit_free_comment_fk FOREIGN KEY(id) REFERENCES hanbit_member(id),
+	CONSTRAINT hanbit_free_comment_no_fk FOREIGN KEY(free_no) REFERENCES hanbit_free_board(free_no)
+)
+CREATE SEQUENCE hanbit_comment_seq;
+DROP TABLE hanbit_comment;
+DROP SEQUENCE hanbit_comment_seq;
+SELECT * FROM hanbit_comment;
+
+-- 한빛 매칭 게시판 테이블 생성
+CREATE TABLE hanbit_match_board(
+	match_no NUMBER PRIMARY KEY,
+	title VARCHAR2(100) NOT NULL,
+	content CLOB NOT NULL,
+	time_posted DATE DEFAULT SYSDATE,
+	hits NUMBER DEFAULT 0,
+	image VARCHAR2(100),
+	id VARCHAR2(100) NOT NULL,
+	CONSTRAINT hanbit_match_board_fk FOREIGN KEY(id) REFERENCES hanbit_member(id)
+)
+CREATE SEQUENCE hanbit_match_board_seq;
+DROP TABLE hanbit_match_board;
+DROP SEQUENCE hanbit_match_board_seq;
+SELECT * FROM hanbit_match_board;
+
+-- 한빛 매칭 좋아요 테이블 생성
+CREATE TABLE hanbit_likes(
+	id VARCHAR2(100),
+	match_no NUMBER,
+	CONSTRAINT hanbit_likes_id_fk FOREIGN KEY(id) REFERENCES hanbit_member(id),
+	CONSTRAINT hanbit_likes_no_fk FOREIGN KEY(match_no) REFERENCES hanbit_match_board(match_no),
+	CONSTRAINT hanbit_likes_pk PRIMARY KEY(id,match_no)
+)
+DROP TABLE hanbit_likes;
+SELECT * FROM hanbit_likes;
+
+-- 한빛 쪽지 테이블 생성 
+CREATE TABLE hanbit_message(
+	message_no NUMBER PRIMARY KEY,
+	content CLOB NOT NULL,
+	checking NUMBER DEFAULT 0,
+	time_posted DATE DEFAULT SYSDATE,
+	send_id VARCHAR2(100) NOT NULL,
+	receive_id VARCHAR2(100) NOT NULL,
+	CONSTRAINT hanbit_message_send_id_fk FOREIGN KEY(send_id) REFERENCES hanbit_member(id),	
+	CONSTRAINT hanbit_message_receive_id_fk FOREIGN KEY(receive_id) REFERENCES hanbit_member(id)
+)
+CREATE SEQUENCE hanbit_message_seq;
+DROP TABLE hanbit_message;
+DROP SEQUENCE hanbit_message_seq;
+SELECT * FROM hanbit_message;
 
 
