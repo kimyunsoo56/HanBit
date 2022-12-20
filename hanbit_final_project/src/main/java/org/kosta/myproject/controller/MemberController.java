666efa2830937181641912254e8b25375d8acd1d
kimyunsoo56 <kimyunsoo56@gmail.com>
2022-12-20 17:17:00 +0900

    김윤수 충돌 해결

--- hanbit_final_project/src/main/java/org/kosta/myproject/controller/MemberController.java
+++ hanbit_final_project/src/main/java/org/kosta/myproject/controller/MemberController.java
@@ -19,14 +19,14 @@ import lombok.RequiredArgsConstructor;
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
-	 private final MemberService memberService;
-	//로그인 폼
-	 @RequestMapping("loginForm")
-	 public String loginForm() {
-		 return "member/login-form";
-	 }
-	 // 로그인
-	 @PostMapping("login")
+    private final MemberService memberService;
+   //로그인 폼
+    @RequestMapping("loginForm")
+    public String loginForm() {
+       return "member/login-form";
+    }
+    // 로그인
+    @PostMapping("/login")
 	 public String login(MemberVO memberVO,HttpServletRequest request) {
 		 MemberVO resultVO=memberService.login(memberVO);
 		 if(resultVO==null) { 
@@ -39,222 +39,222 @@ public class MemberController {
 				return "redirect:/";
 			}
 	 }
-	 // 로그아웃
-	  @PostMapping("logout") 
-			public String logout(HttpServletRequest request) {
-				HttpSession session=request.getSession(false);
-				if(session!=null) 
-					session.invalidate();
-				return "redirect:/";
-	 }
-	  // 마이페이지 
-	  @RequestMapping("myPage")
-		 public String myPage() {
-			 return "member/myPage";
-		 }
-	  // 회원정보상세조회
-	  @GetMapping("/myPageDetail")
-		 public String myPageDetail(HttpServletRequest request,Model model) {
-		  HttpSession session=request.getSession(false);
-		  MemberVO memberVO=(MemberVO) session.getAttribute("mvo");
-		 // String id=memberVO.getId();        
-		  session.setAttribute("mvo", memberVO);
-			 return "member/myPageDetailList";
-	  }
-	// register form 에서 for문 돌릴 select 부분을 model에 담아서 넘겨줌
-	@RequestMapping("registerForm")
-	public String registerForm(Model model) {
-		List<String> question=new ArrayList<>();
-		question.add("가장 기억에 남는 장소는?");
-		question.add("나의 좌우명은?");
-		question.add("나의 보물 제1호는?");
-		question.add("인상 깊게 읽은 책 이름은?");
-		question.add("내가 존경하는 인물은?");
-		question.add("나의 출신 초등학교는?");
-		question.add("나의 노래방 애창곡은?");
-		model.addAttribute("questionList", question);
-		return "member/register-form";
-	}
-	// 회원가입 폼
-	@PostMapping("registerMember")
-	public String register(MemberVO memberVO) {
-		memberService.register(memberVO);
-		return "redirect:registerMemberResult";
-	}
-	// 회원가입
-	@RequestMapping("registerMemberResult")
-	public String registerMemberResult() {
-		return "member/register-result";
-	}
-	// 아이디 중복체크 Ajax
-	@RequestMapping("registerCheckId")
-	@ResponseBody
-	public MemberVO registerCheckId(String id) {
-		MemberVO checkId=memberService.findMemberById(id);
-		return checkId;
-	}
-	// 닉네임 중복체크 Ajax
-	@RequestMapping("registerCheckNick")
-	@ResponseBody
-	public int registerCheckNick(String nick) {
-		int checkNick = memberService.checkNick(nick);
-		return checkNick;
-	}
-	// 연락처 중복체크 Ajax
-	@RequestMapping("registerCheckTel")
-	@ResponseBody
-	public int registerCheckTel(String tel) {
-		int checkTel=memberService.checkTel(tel);
-		return checkTel;
-	}
-	// 아이디 찾기 폼
-	@RequestMapping("findIdForm")
-	public String findIdForm() {
-		return "member/findid-form";
-	} 
-	// 아이디 찾기
-	@RequestMapping("findId")
-	public String findId(String name,String tel,Model model) {
-		String viewName=null;
-		String findId=memberService.findId(name,tel);
-		if(findId==null) {
-			viewName="member/findid-fail";
-		}else {
-			model.addAttribute("memberId", findId);
-			viewName="member/findid-ok";
-		}
-		return viewName;
-	}
-	// 비밀번호 찾기 폼
-	@RequestMapping("findPasswordForm")
-	public String findPasswordForm(Model model) {
-		List<String> question=new ArrayList<>();
-		question.add("가장 기억에 남는 장소는?");
-		question.add("나의 좌우명은?");
-		question.add("나의 보물 제1호는?");
-		question.add("인상 깊게 읽은 책 이름은?");
-		question.add("내가 존경하는 인물은?");
-		question.add("나의 출신 초등학교는?");
-		question.add("나의 노래방 애창곡은?");
-		model.addAttribute("questionList", question);
-		return "member/findpassword-form";
-	}
-	// 비밀번호 찾기
-	@RequestMapping("findPassword")
-	public String findPassword(String id,String name,String tel,String question,String answer,Model model) {
-		String viewName=null;
-		String findPassword=memberService.findPassword(id,name,tel,question,answer);
-		if(findPassword==null) {
-			viewName="member/findpassword-fail";
-		}else {
-			model.addAttribute("memberPassword", findPassword);
-			viewName="member/findpassword-ok";
-		}
-		return viewName;
-	}
-	// 요양보호사 등록 폼
-	@RequestMapping("registerCareWorkerForm")
-	public String registerCareWorkerForm(Model model) {
-		// 성별
-		List<String> gender=new ArrayList<>();
-		gender.add("남성");
-		gender.add("여성");
-		model.addAttribute("genderList", gender);
-		// 경력
-		List<String> workHistory=new ArrayList<>();
-		workHistory.add("신입");
-		workHistory.add("1년 미만");
-		workHistory.add("1년~3년");
-		workHistory.add("3년~5년");
-		workHistory.add("5~7년");
-		workHistory.add("7년~10년");
-		workHistory.add("10년 이상");
-		model.addAttribute("workHistoryList", workHistory);
-		// 지역
-		List<String> location=new ArrayList<>();
-		location.add("서울");
-		location.add("경기");
-		location.add("강원");
-		location.add("충북");
-		location.add("충남");
-		location.add("전북");
-		location.add("전남");
-		location.add("경북");
-		location.add("경남");
-		location.add("제주");
-		model.addAttribute("locationList", location);
-		// 근무타입
-		List<String> workType=new ArrayList<>();
-		workType.add("자택근무");
-		workType.add("병원근무");
-		model.addAttribute("workTypeList", workType);
-		return "member/register-careworker-form";
-	}
-	 @RequestMapping("deleteMemberForm")
-	 public String deleteMemberForm(Model model) {
-		 List<String> delectquestion=new ArrayList<>();
-		 delectquestion.add("가장 기억에 남는 장소는?");
-		 delectquestion.add("나의 좌우명은?");
-		 delectquestion.add("나의 보물 제1호는?");
-		 delectquestion.add("인상 깊게 읽은 책 이름은?");
-		 delectquestion.add("내가 존경하는 인물은?");
-		 delectquestion.add("나의 출신 초등학교는?");
-		 delectquestion.add("나의 노래방 애창곡은?");
-		 model.addAttribute("delectquestionList", delectquestion);
-		 return "member/deleteMember-form";
-	 }
-	 // 회원탈퇴 쿼리문은 메서드명은 회원 탈퇴라 delect이고 쿼리문은 update 
-		/*
-		 * @PostMapping("deleteMember") public String deleteMember(MemberVO memberVO,
-		 * HttpServletRequest request) { MemberService.delectMember(memberVO); MemberVO
-		 * memberVO=(MemberVO) session.getAttribute("mvo"); HttpSession
-		 * session=request.getSession(false); if() {
-		 * 
-		 * }else {
-		 * 
-		 * return "member/deleteMember-result"; } }
-		 */
-	// 회원 정보 수정 폼
-		@RequestMapping("updateMemberForm")
-		public String updateMemberForm(Model model) {
-			// 개별질문
-			List<String> question = new ArrayList<>();
-			question.add("가장 기억에 남는 장소는?");
-			question.add("나의 좌우명은?");
-			question.add("나의 보물 제1호는?");
-			question.add("인상 깊게 읽은 책 이름은?");
-			question.add("내가 존경하는 인물은?");
-			question.add("나의 출신 초등학교는?");
-			question.add("나의 노래방 애창곡은?");
-			model.addAttribute("questionList", question);
-			// 경력
-			List<String> workHistory = new ArrayList<>();
-			workHistory.add("신입");
-			workHistory.add("1년 미만");
-			workHistory.add("1년~3년");
-			workHistory.add("3년~5년");
-			workHistory.add("5~7년");
-			workHistory.add("7년~10년");
-			workHistory.add("10년 이상");
-			model.addAttribute("workHistoryList", workHistory);
-			// 지역
-			List<String> location = new ArrayList<>();
-			location.add("서울");
-			location.add("경기");
-			location.add("강원");
-			location.add("충북");
-			location.add("충남");
-			location.add("전북");
-			location.add("전남");
-			location.add("경북");
-			location.add("경남");
-			location.add("제주");
-			model.addAttribute("locationList", location);
-			// 근무형태
-			List<String> workType = new ArrayList<>();
-			workType.add("자택근무");
-			workType.add("병원근무");
-			model.addAttribute("workTypeList", workType);
-			return "member/update-member-form";
-		}
+    // 로그아웃
+     @PostMapping("logout") 
+         public String logout(HttpServletRequest request) {
+            HttpSession session=request.getSession(false);
+            if(session!=null) 
+               session.invalidate();
+            return "redirect:/";
+    }
+     // 마이페이지 
+     @RequestMapping("myPage")
+       public String myPage() {
+          return "member/myPage";
+       }
+     // 회원정보상세조회
+     @GetMapping("/myPageDetail")
+       public String myPageDetail(HttpServletRequest request,Model model) {
+        HttpSession session=request.getSession(false);
+        MemberVO memberVO=(MemberVO) session.getAttribute("mvo");
+       // String id=memberVO.getId();        
+        session.setAttribute("mvo", memberVO);
+          return "member/myPageDetailList";
+     }
+   // register form 에서 for문 돌릴 select 부분을 model에 담아서 넘겨줌
+   @RequestMapping("registerForm")
+   public String registerForm(Model model) {
+      List<String> question=new ArrayList<>();
+      question.add("가장 기억에 남는 장소는?");
+      question.add("나의 좌우명은?");
+      question.add("나의 보물 제1호는?");
+      question.add("인상 깊게 읽은 책 이름은?");
+      question.add("내가 존경하는 인물은?");
+      question.add("나의 출신 초등학교는?");
+      question.add("나의 노래방 애창곡은?");
+      model.addAttribute("questionList", question);
+      return "member/register-form";
+   }
+   // 회원가입 폼
+   @PostMapping("registerMember")
+   public String register(MemberVO memberVO) {
+      memberService.register(memberVO);
+      return "redirect:registerMemberResult";
+   }
+   // 회원가입
+   @RequestMapping("registerMemberResult")
+   public String registerMemberResult() {
+      return "member/register-result";
+   }
+   // 아이디 중복체크 Ajax
+   @RequestMapping("registerCheckId")
+   @ResponseBody
+   public MemberVO registerCheckId(String id) {
+      MemberVO checkId=memberService.findMemberById(id);
+      return checkId;
+   }
+   // 닉네임 중복체크 Ajax
+   @RequestMapping("registerCheckNick")
+   @ResponseBody
+   public int registerCheckNick(String nick) {
+      int checkNick = memberService.checkNick(nick);
+      return checkNick;
+   }
+   // 연락처 중복체크 Ajax
+   @RequestMapping("registerCheckTel")
+   @ResponseBody
+   public int registerCheckTel(String tel) {
+      int checkTel=memberService.checkTel(tel);
+      return checkTel;
+   }
+   // 아이디 찾기 폼
+   @RequestMapping("findIdForm")
+   public String findIdForm() {
+      return "member/findid-form";
+   } 
+   // 아이디 찾기
+   @RequestMapping("findId")
+   public String findId(String name,String tel,Model model) {
+      String viewName=null;
+      String findId=memberService.findId(name,tel);
+      if(findId==null) {
+         viewName="member/findid-fail";
+      }else {
+         model.addAttribute("memberId", findId);
+         viewName="member/findid-ok";
+      }
+      return viewName;
+   }
+   // 비밀번호 찾기 폼
+   @RequestMapping("findPasswordForm")
+   public String findPasswordForm(Model model) {
+      List<String> question=new ArrayList<>();
+      question.add("가장 기억에 남는 장소는?");
+      question.add("나의 좌우명은?");
+      question.add("나의 보물 제1호는?");
+      question.add("인상 깊게 읽은 책 이름은?");
+      question.add("내가 존경하는 인물은?");
+      question.add("나의 출신 초등학교는?");
+      question.add("나의 노래방 애창곡은?");
+      model.addAttribute("questionList", question);
+      return "member/findpassword-form";
+   }
+   // 비밀번호 찾기
+   @RequestMapping("findPassword")
+   public String findPassword(String id,String name,String tel,String question,String answer,Model model) {
+      String viewName=null;
+      String findPassword=memberService.findPassword(id,name,tel,question,answer);
+      if(findPassword==null) {
+         viewName="member/findpassword-fail";
+      }else {
+         model.addAttribute("memberPassword", findPassword);
+         viewName="member/findpassword-ok";
+      }
+      return viewName;
+   }
+   // 요양보호사 등록 폼
+   @RequestMapping("registerCareWorkerForm")
+   public String registerCareWorkerForm(Model model) {
+      // 성별
+      List<String> gender=new ArrayList<>();
+      gender.add("남성");
+      gender.add("여성");
+      model.addAttribute("genderList", gender);
+      // 경력
+      List<String> workHistory=new ArrayList<>();
+      workHistory.add("신입");
+      workHistory.add("1년 미만");
+      workHistory.add("1년~3년");
+      workHistory.add("3년~5년");
+      workHistory.add("5~7년");
+      workHistory.add("7년~10년");
+      workHistory.add("10년 이상");
+      model.addAttribute("workHistoryList", workHistory);
+      // 지역
+      List<String> location=new ArrayList<>();
+      location.add("서울");
+      location.add("경기");
+      location.add("강원");
+      location.add("충북");
+      location.add("충남");
+      location.add("전북");
+      location.add("전남");
+      location.add("경북");
+      location.add("경남");
+      location.add("제주");
+      model.addAttribute("locationList", location);
+      // 근무타입
+      List<String> workType=new ArrayList<>();
+      workType.add("자택근무");
+      workType.add("병원근무");
+      model.addAttribute("workTypeList", workType);
+      return "member/register-careworker-form";
+   }
+    @RequestMapping("deleteMemberForm")
+    public String deleteMemberForm(Model model) {
+       List<String> delectquestion=new ArrayList<>();
+       delectquestion.add("가장 기억에 남는 장소는?");
+       delectquestion.add("나의 좌우명은?");
+       delectquestion.add("나의 보물 제1호는?");
+       delectquestion.add("인상 깊게 읽은 책 이름은?");
+       delectquestion.add("내가 존경하는 인물은?");
+       delectquestion.add("나의 출신 초등학교는?");
+       delectquestion.add("나의 노래방 애창곡은?");
+       model.addAttribute("delectquestionList", delectquestion);
+       return "member/deleteMember-form";
+    }
+    // 회원탈퇴 쿼리문은 메서드명은 회원 탈퇴라 delect이고 쿼리문은 update 
+      /*
+       * @PostMapping("deleteMember") public String deleteMember(MemberVO memberVO,
+       * HttpServletRequest request) { MemberService.delectMember(memberVO); MemberVO
+       * memberVO=(MemberVO) session.getAttribute("mvo"); HttpSession
+       * session=request.getSession(false); if() {
+       * 
+       * }else {
+       * 
+       * return "member/deleteMember-result"; } }
+       */
+   // 회원 정보 수정 폼
+      @RequestMapping("updateMemberForm")
+      public String updateMemberForm(Model model) {
+         // 개별질문
+         List<String> question = new ArrayList<>();
+         question.add("가장 기억에 남는 장소는?");
+         question.add("나의 좌우명은?");
+         question.add("나의 보물 제1호는?");
+         question.add("인상 깊게 읽은 책 이름은?");
+         question.add("내가 존경하는 인물은?");
+         question.add("나의 출신 초등학교는?");
+         question.add("나의 노래방 애창곡은?");
+         model.addAttribute("questionList", question);
+         // 경력
+         List<String> workHistory = new ArrayList<>();
+         workHistory.add("신입");
+         workHistory.add("1년 미만");
+         workHistory.add("1년~3년");
+         workHistory.add("3년~5년");
+         workHistory.add("5~7년");
+         workHistory.add("7년~10년");
+         workHistory.add("10년 이상");
+         model.addAttribute("workHistoryList", workHistory);
+         // 지역
+         List<String> location = new ArrayList<>();
+         location.add("서울");
+         location.add("경기");
+         location.add("강원");
+         location.add("충북");
+         location.add("충남");
+         location.add("전북");
+         location.add("전남");
+         location.add("경북");
+         location.add("경남");
+         location.add("제주");
+         model.addAttribute("locationList", location);
+         // 근무형태
+         List<String> workType = new ArrayList<>();
+         workType.add("자택근무");
+         workType.add("병원근무");
+         model.addAttribute("workTypeList", workType);
+         return "member/update-member-form";
+      }
 }
