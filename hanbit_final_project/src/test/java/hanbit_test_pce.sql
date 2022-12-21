4d159aa26364d1945aac7c38e947093a268129fb
chaen-ni <cossltkfkd77@naver.com>
2022-12-21 17:45:26 +0900

    박채은-작성 글 목록 조회 Sql Test

--- hanbit_final_project/src/test/java/hanbit_test_pce.sql
+++ hanbit_final_project/src/test/java/hanbit_test_pce.sql
@@ -2,21 +2,34 @@ SELECT * FROM hanbit_member;
 
 SELECT id,password,name,nick,question,answer,tel,enabled,member_type,license_no,gender,work_history,location,work_type
 FROM hanbit_member WHERE id='java';
-
-INSERT INTO hanbit_member(id,password,name,nick,question,answer,tel) VALUES('java','a','이지은','아이유','나의 노래방 애창곡은?','좋은날','01012345678');
-
 DELETE FROM hanbit_member WHERE id='7777'; 
 
+-- 회원가입
+INSERT INTO hanbit_member(id,password,name,nick,question,answer,tel) VALUES('java','a','이지은','아이유','나의 노래방 애창곡은?','좋은날','01012345678');
+
+-- 닉네임 중복확인
 SELECT COUNT(*) FROM hanbit_member WHERE nick='아이유';
 
+-- 아이디 찾기
 SELECT id FROM hanbit_member WHERE name='박진수' AND tel='01028355343';
 
+-- 비밀번호 찾기
 SELECT password FROM hanbit_member WHERE id='spring' AND name='박진수' AND tel='01028355343' AND question='나의 출신 초등학교는?' AND answer='문성초';
 
+-- 자격증 번호 중복확인
 SELECT COUNT(*) FROM hanbit_member WHERE license_no=1111
 
+-- 요양보호사 등록
 UPDATE hanbit_member SET member_type=2,license_no=1111111111,
 gender='여성',work_history='신입',location='서울',work_type='자택근무' WHERE id='1111';
 
+-- 회원 정보 수정
 UPDATE hanbit_member SET password='a',name='박범',nick='범선생',question='가장 기억에 남는 장소는?',answer='오리',
 tel='01098765432',license_no=1234567,gender='남성',work_history='신입',location='경기',work_type='자택근무' WHERE id='java3';
+
+-- 작성 글 목록 
+SELECT nb.title,nb.content,nb.time_posted,nb.hits,nb.image,hm.id,hm.name
+FROM hanbit_notice_board nb
+INNER JOIN hanbit_member hm ON nb.id=hm.id
+WHERE hm.id='admin'
+
